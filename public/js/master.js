e55862e968204cf246441fd16a0454045755ce9e
daltonvarao <daltonphellipe@gmail.com>
2020-08-14 15:46:30 -0300

    ajusta footer

--- public/js/master.js
+++ public/js/master.js
@@ -1,55 +1,60 @@
 // navigation menu
-const closeButtonNav = document.querySelector("#close-menu-nav");
-const showButtonNav = document.querySelector("#show-menu-nav");
 
-const menuNav = document.querySelector(".nav-menu");
+window.onload = function () {
+  const closeButtonNav = document.querySelector("#close-menu-nav");
+  const showButtonNav = document.querySelector("#show-menu-nav");
 
-closeButtonNav.onclick = function () {
-  menuNav.classList.replace("show", "hidden");
-};
+  const menuNav = document.querySelector(".nav-menu");
 
-showButtonNav.onclick = function () {
-  menuNav.classList.replace("hidden", "show");
-};
-
-// alert messages
-const closeAlertBtn = document.querySelector(".alert-close");
-const alertMessage = document.querySelector(".alert-message");
-
-if (closeAlertBtn) {
-  closeAlertBtn.onclick = function () {
-    alertMessage.classList.add("hidden");
-  };
-}
-
-// dropdown menu
-const dropdownButton = document.querySelector(".dropdown");
-const dropdownContent = document.querySelector(".dropdown-content");
-
-if (dropdownButton) {
-  dropdownButton.onclick = function () {
-    if (dropdownContent.classList.contains("hidden")) {
-      dropdownContent.classList.replace("hidden", "show");
-    } else {
-      dropdownContent.classList.replace("show", "hidden");
-    }
+  closeButtonNav.onclick = function () {
+    menuNav.classList.replace("show", "hidden");
   };
 
-  window.onclick = function (ev) {
-    if (!ev.target.matches(".dropdown-button"))
-      dropdownContent.classList.replace("show", "hidden");
+  showButtonNav.onclick = function () {
+    menuNav.classList.replace("hidden", "show");
   };
-}
 
-// delete prompt
-const deleteButtons = document.querySelectorAll(".delete-button");
+  // alert messages
+  const closeAlertBtn = document.querySelector(".alert-close");
+  const alertMessage = document.querySelector(".alert-message");
 
-if (deleteButtons) {
-  deleteButtons.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      if (confirm("Deseja realmente deletar este item?")) {
-        btn.parentElement.submit();
+  if (closeAlertBtn) {
+    closeAlertBtn.onclick = function () {
+      alertMessage.classList.add("hidden");
+    };
+  }
+
+  // dropdown menu
+  const dropdownButton = document.querySelector(".dropdown");
+  const dropdownContent = document.querySelector(".dropdown-content");
+
+  if (dropdownButton) {
+    dropdownButton.onclick = function () {
+      if (dropdownContent.classList.contains("hidden")) {
+        dropdownContent.classList.replace("hidden", "show");
+      } else {
+        dropdownContent.classList.replace("show", "hidden");
       }
+    };
+
+    window.onclick = function (ev) {
+      if (!ev.target.matches(".dropdown-button"))
+        dropdownContent.classList.replace("show", "hidden");
+    };
+  }
+
+  // delete prompt
+  const deleteButtons = document.querySelectorAll(".delete-button");
+
+  if (deleteButtons) {
+    deleteButtons.forEach(function (btn) {
+      btn.addEventListener("click", function () {
+        if (confirm("Deseja realmente deletar este item?")) {
+          btn.parentElement.submit();
+        }
+      });
     });
-  });
-}
+  }
+
+  document.querySelector("#current-year").innerHTML = new Date().getFullYear();
+};
