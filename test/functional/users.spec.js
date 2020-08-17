d802f9b393f5e9e4940441566837be99b7b1dbfd
daltonvarao <daltonphellipe@gmail.com>
2020-08-17 09:51:00 -0300

    WIP: fix user tests

--- test/functional/users.spec.js
+++ test/functional/users.spec.js
@@ -12,30 +12,35 @@ const User = use("App/Models/User");
 test("should be able to access user index page in GET /admin/users", async ({
   browser,
 }) => {
-  const user = await User.findBy("email", "admin@admin.com");
-  const response = await browser.visit("/admin/users").loginVia(user);
+  const user = await User.find(1);
+  const response = await browser.visit("/admin/users", (request) => {
+    request.loginVia(user);
+  });
 
   await response.assertPath("/admin/users");
   await response.assertHas("Usuários");
 }).timeout(0);
 
-// test("should be able to create a user in POST /admin/users/store", async ({
-//   assert,
-//   browser,
-// }) => {
-//   const response = await browser.visit("/admin/users/create");
+test("should be able to create a user in POST /admin/users/store", async ({
+  assert,
+  browser,
+}) => {
+  const user = await User.find(1);
+  const response = await browser.visit("/admin/users/create", (request) => {
+    request.loginVia(user);
+  });
 
-//   await response.assertHas("Novo Usuário");
+  await response.assertHas("Novo Usuário");
 
-//   await response
-//     .type('[name="name"]', "Dalton Felipe")
-//     .type('[name="email"]', "daltonvarao@gmail.com")
-//     .type('[name="password"]', "12345678")
-//     .type('[name="password_confirmation"]', "12345678")
-//     .check('[name="is_admin"]')
-//     .submitForm("form");
+  await response
+    .type('[name="name"]', "Dalton Felipe")
+    .type('[name="email"]', "daltonvarao@gmail.com")
+    .type('[name="password"]', "12345678")
+    .type('[name="password_confirmation"]', "12345678")
+    .check('[name="is_admin"]')
+    .submitForm("form");
 
-//   const user = await User.findBy("email", "daltonvarao@gmail.com");
+  const newUser = await User.findBy("email", "daltonvarao@gmail.com");
 
-//   assert.equal(user.name, "Dalton Felipe");
-// });
+  assert.equal(newUser.name, "Dalton Felipe");
+});
