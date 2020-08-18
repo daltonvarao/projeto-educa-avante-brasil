7e37ae06da630c513d314f2b27b15066835ab539
daltonvarao <daltonphellipe@gmail.com>
2020-08-18 18:13:49 -0300

    ajusta pagina de modalidades

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const AreaEstudo = use("App/Models/AreaEstudo");
+const Modalidade = use("App/Models/Modalidade");
 
 class HomeController {
   async index({ view }) {
     const areas = await AreaEstudo.all();
+    const modalidades = await Modalidade.all();
 
     return view.render("home.index", {
       areas: areas.toJSON(),
+      modalidades: modalidades.toJSON(),
     });
   }
 }
