6c61e2a10eeec0beeb820981b92d381e2daf9759
daltonvarao <daltonphellipe@gmail.com>
2020-08-19 12:03:59 -0300

    WIP: add model e migrations

---
+++ database/migrations/1597785266288_curso_schema.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CursoSchema extends Schema {
+  up () {
+    this.create('cursos', (table) => {
+      table.increments()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('cursos')
+  }
+}
+
+module.exports = CursoSchema
