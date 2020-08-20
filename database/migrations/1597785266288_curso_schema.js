a8d7ba5b69ae32f839c2cf2a26c36a7a24a39f7f
daltonvarao <daltonphellipe@gmail.com>
2020-08-20 10:38:59 -0300

    add migration de curso

--- database/migrations/1597785266288_curso_schema.js
+++ database/migrations/1597785266288_curso_schema.js
@@ -1,19 +1,37 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class CursoSchema extends Schema {
-  up () {
-    this.create('cursos', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("cursos", (table) => {
+      table.increments();
+
+      table.string("nome").notNullable();
+      table.string("sobre").notNullable();
+      table.string("instituicao").notNullable();
+      table.string("tipo").notNullable();
+      table.string("duracao").notNullable();
+
+      table
+        .integer("area_estudo_id")
+        .unsigned()
+        .references("id")
+        .inTable("area_estudos");
+      table
+        .integer("modalidade_id")
+        .unsigned()
+        .references("id")
+        .inTable("modalidades");
+
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('cursos')
+  down() {
+    this.drop("cursos");
   }
 }
 
-module.exports = CursoSchema
+module.exports = CursoSchema;
