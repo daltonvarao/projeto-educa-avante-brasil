d6acfd6850bd829317dd098fb84a583653c1c10f
daltonvarao <daltonphellipe@gmail.com>
2020-08-31 18:25:56 -0300

    wip: add model e migration de forma_pagamentos

---
+++ database/migrations/1598893365030_forma_pagamento_schema.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use("Schema");
+
+class FormaPagamentoSchema extends Schema {
+  up() {
+    this.create("forma_pagamentos", (table) => {
+      table.increments();
+      table.integer("parcelas").defaultTo(1);
+      table.decimal("valor_parcela").defaultTo(0.0);
+      table.decimal("desconto_parcela").defaultTo(0.0);
+      table.decimal("valor_total").defaultTo(0.0);
+      table.decimal("valor_liquido").defaultTo(0.0);
+      table.enu("forma_pagamento", ["boleto", "cartao"]);
+      table.string("conclusao");
+
+      table.integer("curso_id").unsigned().references("id").inTable("users");
+
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop("forma_pagamentos");
+  }
+}
+
+module.exports = FormaPagamentoSchema;
