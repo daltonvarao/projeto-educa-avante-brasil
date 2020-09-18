ac16538c26a6e1aebe9140a27d91fcef3864a5cd
daltonvarao <daltonphellipe@gmail.com>
2020-09-18 12:06:18 -0300

    finaliza envio de email

--- resources/assets/src/price_box.js
+++ resources/assets/src/price_box.js
@@ -60,7 +60,7 @@ function SubscriptionModal({
     forma_pagamento_id: selected,
     accept_contract: false,
     aceita_contato: false,
-    completed: true,
+    completed: false,
     curso_id: cursoId,
     loading: false,
   };
