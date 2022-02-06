99b9f4f0a66b25feb9a1773585bdede84da5736b
Dalton Varao <dalton@eurekalabs.com.br>
2022-02-06 10:13:35 -0300

    ajusta database config

--- config/database.js
+++ config/database.js
@@ -72,14 +72,12 @@ module.exports = {
   */
   pg: {
     client: "pg",
-    connection: ["development", "testing"].includes(Env.get("NODE_ENV"))
-      ? {
-          host: Env.get("DB_HOST", "localhost"),
-          port: Env.get("DB_PORT", ""),
-          user: Env.get("DB_USER", "root"),
-          password: Env.get("DB_PASSWORD", ""),
-          database: Env.get("DB_DATABASE", "adonis"),
-        }
-      : Env.getOrFail("DATABASE_URL"),
+    connection: {
+      host: Env.get("DB_HOST", "localhost"),
+      port: Env.get("DB_PORT", "5432"),
+      user: Env.get("DB_USER", "root"),
+      password: Env.get("DB_PASSWORD", ""),
+      database: Env.get("DB_DATABASE", "adonis"),
+    },
   },
 };
